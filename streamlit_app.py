2249a176200b94767aa595eb8dfa11d86cede1e7
cassioleguizamonbueno <36665958+cassioleguizamonbueno@users.noreply.github.com>
2025-10-03 12:00:12 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 from datetime import datetime
+from st_aggrid import AgGrid, GridOptionsBuilder
 
 st.set_page_config(layout="wide")
 
@@ -8,41 +9,37 @@ uploaded_files = st.file_uploader("Choose a file", accept_multiple_files=True)
 xls = []
 sheet = []
 sheet_names = []
+
 for i, uploaded_file in enumerate(uploaded_files):
     if uploaded_file is not None:
-        st.write(f"""
-        # Arquivo: {uploaded_file.name}
-        """)
-        if uploaded_file.name[-3:] == 'csv':
+        st.write(f"### Arquivo: {uploaded_file.name}")
+
+        if uploaded_file.name.endswith("csv"):
             dataframe = pd.read_csv(uploaded_file, sep=None, engine='python', dtype=str)
-            #dataframe = pd.read_csv(uploaded_file, sep='^', index_col=False, engine='python', dtype=str)
-            #dataframe = pd.read_csv(uploaded_file, sep=";")
-            st.write(dataframe)
+            # Usando AgGrid no lugar de st.write
+            gb = GridOptionsBuilder.from_dataframe(dataframe)
+            gb.configure_pagination(paginationAutoPageSize=True)  # paginação automática
+            gb.configure_side_bar()  # barra lateral com filtros
+            gridOptions = gb.build()
+            AgGrid(dataframe, gridOptions=gridOptions, height=500, fit_columns_on_grid_load=True)
 
-        elif uploaded_file.name[-7:] == 'parquet':
+        elif uploaded_file.name.endswith("parquet"):
             dataframe = pd.read_parquet(uploaded_file)
-            #caminho = '/home/cassio/Downloads/'
+            gb = GridOptionsBuilder.from_dataframe(dataframe)
+            gb.configure_pagination(paginationAutoPageSize=True)
+            gb.configure_side_bar()
+            gridOptions = gb.build()
+            AgGrid(dataframe, gridOptions=gridOptions, height=500, fit_columns_on_grid_load=True)
 
-            #data = datetime.now().strftime('%Y-%m-%d %H')
-            #file_parquet = f"{caminho}/plurix-{data}.xlsx"
-            #dataframe.to_excel(file_parquet, index=False)
-
-            st.write(dataframe)
-
-            
-
-
-        elif uploaded_file.name[-4:] == 'xlsx':
-
-            xls.append(pd.ExcelFile(uploaded_file))  # Usar append para adicionar o ExcelFile
-            sheet_names.append(xls[i].sheet_names)  # Adicionar as abas
-
-            # Selecionar uma aba para o arquivo atual
+        elif uploaded_file.name.endswith("xlsx"):
+            xls.append(pd.ExcelFile(uploaded_file))
+            sheet_names.append(xls[i].sheet_names)
             sheet.append(st.selectbox(f"Escolha uma aba para o arquivo {uploaded_file.name}", sheet_names[i]))
-
-            # Ler os dados da aba selecionada
             df = pd.read_excel(uploaded_file, sheet_name=sheet[i])
 
-            # Mostrar os dados da aba selecionada
             st.write(f"Dados da aba {sheet[i]}:")
-            st.write(df)
+            gb = GridOptionsBuilder.from_dataframe(df)
+            gb.configure_pagination(paginationAutoPageSize=True)
+            gb.configure_side_bar()
+            gridOptions = gb.build()
+            AgGrid(df, gridOptions=gridOptions, height=500, fit_columns_on_grid_load=True)
