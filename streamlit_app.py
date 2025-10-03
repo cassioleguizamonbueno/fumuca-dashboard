7c471359009aec7e1f5be083e95d95ba6db6a8af
cassioleguizamonbueno <36665958+cassioleguizamonbueno@users.noreply.github.com>
2025-10-03 11:54:37 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,151 +1,48 @@
 import streamlit as st
 import pandas as pd
-import math
-from pathlib import Path
+from datetime import datetime
 
-# Set the title and favicon that appear in the Browser's tab bar.
-st.set_page_config(
-    page_title='GDP dashboard',
-    page_icon=':earth_americas:', # This is an emoji shortcode. Could be a URL too.
-)
+st.set_page_config(layout="wide")
 
-# -----------------------------------------------------------------------------
-# Declare some useful functions.
+uploaded_files = st.file_uploader("Choose a file", accept_multiple_files=True)
+xls = []
+sheet = []
+sheet_names = []
+for i, uploaded_file in enumerate(uploaded_files):
+    if uploaded_file is not None:
+        st.write(f"""
+        # Arquivo: {uploaded_file.name}
+        """)
+        if uploaded_file.name[-3:] == 'csv':
+            dataframe = pd.read_csv(uploaded_file, sep=None, engine='python', dtype=str)
+            #dataframe = pd.read_csv(uploaded_file, sep='^', index_col=False, engine='python', dtype=str)
+            #dataframe = pd.read_csv(uploaded_file, sep=";")
+            st.write(dataframe)
 
-@st.cache_data
-def get_gdp_data():
-    """Grab GDP data from a CSV file.
+        elif uploaded_file.name[-7:] == 'parquet':
+            dataframe = pd.read_parquet(uploaded_file)
+            #caminho = '/home/cassio/Downloads/'
 
-    This uses caching to avoid having to read the file every time. If we were
-    reading from an HTTP endpoint instead of a file, it's a good idea to set
-    a maximum age to the cache with the TTL argument: @st.cache_data(ttl='1d')
-    """
+            #data = datetime.now().strftime('%Y-%m-%d %H')
+            #file_parquet = f"{caminho}/plurix-{data}.xlsx"
+            #dataframe.to_excel(file_parquet, index=False)
 
-    # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
-    DATA_FILENAME = Path(__file__).parent/'data/gdp_data.csv'
-    raw_gdp_df = pd.read_csv(DATA_FILENAME)
+            st.write(dataframe)
 
-    MIN_YEAR = 1960
-    MAX_YEAR = 2022
-
-    # The data above has columns like:
-    # - Country Name
-    # - Country Code
-    # - [Stuff I don't care about]
-    # - GDP for 1960
-    # - GDP for 1961
-    # - GDP for 1962
-    # - ...
-    # - GDP for 2022
-    #
-    # ...but I want this instead:
-    # - Country Name
-    # - Country Code
-    # - Year
-    # - GDP
-    #
-    # So let's pivot all those year-columns into two: Year and GDP
-    gdp_df = raw_gdp_df.melt(
-        ['Country Code'],
-        [str(x) for x in range(MIN_YEAR, MAX_YEAR + 1)],
-        'Year',
-        'GDP',
-    )
-
-    # Convert years from string to integers
-    gdp_df['Year'] = pd.to_numeric(gdp_df['Year'])
-
-    return gdp_df
-
-gdp_df = get_gdp_data()
-
-# -----------------------------------------------------------------------------
-# Draw the actual page
-
-# Set the title that appears at the top of the page.
-'''
-# :earth_americas: GDP dashboard
-
-Browse GDP data from the [World Bank Open Data](https://data.worldbank.org/) website. As you'll
-notice, the data only goes to 2022 right now, and datapoints for certain years are often missing.
-But it's otherwise a great (and did I mention _free_?) source of data.
-'''
-
-# Add some spacing
-''
-''
-
-min_value = gdp_df['Year'].min()
-max_value = gdp_df['Year'].max()
-
-from_year, to_year = st.slider(
-    'Which years are you interested in?',
-    min_value=min_value,
-    max_value=max_value,
-    value=[min_value, max_value])
-
-countries = gdp_df['Country Code'].unique()
-
-if not len(countries):
-    st.warning("Select at least one country")
-
-selected_countries = st.multiselect(
-    'Which countries would you like to view?',
-    countries,
-    ['DEU', 'FRA', 'GBR', 'BRA', 'MEX', 'JPN'])
-
-''
-''
-''
-
-# Filter the data
-filtered_gdp_df = gdp_df[
-    (gdp_df['Country Code'].isin(selected_countries))
-    & (gdp_df['Year'] <= to_year)
-    & (from_year <= gdp_df['Year'])
-]
-
-st.header('GDP over time', divider='gray')
-
-''
-
-st.line_chart(
-    filtered_gdp_df,
-    x='Year',
-    y='GDP',
-    color='Country Code',
-)
-
-''
-''
+            
 
 
-first_year = gdp_df[gdp_df['Year'] == from_year]
-last_year = gdp_df[gdp_df['Year'] == to_year]
+        elif uploaded_file.name[-4:] == 'xlsx':
 
-st.header(f'GDP in {to_year}', divider='gray')
+            xls.append(pd.ExcelFile(uploaded_file))  # Usar append para adicionar o ExcelFile
+            sheet_names.append(xls[i].sheet_names)  # Adicionar as abas
 
-''
+            # Selecionar uma aba para o arquivo atual
+            sheet.append(st.selectbox(f"Escolha uma aba para o arquivo {uploaded_file.name}", sheet_names[i]))
 
-cols = st.columns(4)
+            # Ler os dados da aba selecionada
+            df = pd.read_excel(uploaded_file, sheet_name=sheet[i])
 
-for i, country in enumerate(selected_countries):
-    col = cols[i % len(cols)]
-
-    with col:
-        first_gdp = first_year[first_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
-        last_gdp = last_year[last_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
-
-        if math.isnan(first_gdp):
-            growth = 'n/a'
-            delta_color = 'off'
-        else:
-            growth = f'{last_gdp / first_gdp:,.2f}x'
-            delta_color = 'normal'
-
-        st.metric(
-            label=f'{country} GDP',
-            value=f'{last_gdp:,.0f}B',
-            delta=growth,
-            delta_color=delta_color
-        )
+            # Mostrar os dados da aba selecionada
+            st.write(f"Dados da aba {sheet[i]}:")
+            st.write(df)
